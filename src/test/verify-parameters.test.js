bc1ed33d0712a3f4111bea68755170b7ab9597dc
Kyle Angelo Galendez <chessurisme@gmail.com>
2025-01-21 14:48:31 +0800

    test: add unit tests for verifyParameters function

---
+++ src/test/verify-parameters.test.js
@@ -0,0 +1,33 @@
+import { verifyParameters } from '../verify-parameters';
+
+describe('verifyParameters', () => {
+	describe('databaseName', () => {
+		it('should throw an error if databaseName is not a string', () => {
+			expect(() => verifyParameters(1, 'store', 1)).toThrow('Database name is required and must be a string');
+		});
+
+		it('should throw an error if databaseName is not provided', () => {
+			expect(() => verifyParameters(undefined, 'store', 1)).toThrow('Database name is required and must be a string');
+		});
+	});
+
+	describe('storeName', () => {
+		it('should throw an error if storeName is not a string', () => {
+			expect(() => verifyParameters('db', 1, 1)).toThrow('Store name is required and must be a string');
+		});
+
+		it('should throw an error if storeName is not provided', () => {
+			expect(() => verifyParameters('db', undefined, 1)).toThrow('Store name is required and must be a string');
+		});
+	});
+
+	describe('version', () => {
+		it('should throw an error if version is not a number', () => {
+			expect(() => verifyParameters('db', 'store', '1')).toThrow('Version is required and must be a number');
+		});
+
+		it('should throw an error if version is not provided', () => {
+			expect(() => verifyParameters('db', 'store', undefined)).toThrow('Version is required and must be a number');
+		});
+	});
+});
