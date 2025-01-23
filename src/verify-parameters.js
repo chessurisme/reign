3392fd72cb35b2b5d6ffcc607b5030f1087c4f3c
Kyle Angelo Galendez <chessurisme@gmail.com>
2025-01-23 15:30:55 +0800

    fix: update parameter verification to accept an array of store names

--- src/verify-parameters.js
+++ src/verify-parameters.js
@@ -2,7 +2,7 @@
  * Verifies the parameters passed to the Reign constructor.
  *
  * @param {String} databaseName - The name of the database
- * @param {String} storeName - The name of the store
+ * @param {Array} storeName - The name of the store
  * @param {Number} version - The version of the store
  * @throws {Error} If any of the parameters is invalid
  */
@@ -10,9 +10,11 @@ function verifyParameters(databaseName, storeName, version) {
 	if (!databaseName || typeof databaseName !== 'string') {
 		throw new Error('Database name is required and must be a string');
 	}
-	if (!storeName || typeof storeName !== 'string') {
-		throw new Error('Store name is required and must be a string');
+
+	if (!Array.isArray(storeName) || storeName.length === 0 || !storeName.every((name) => typeof name === 'string')) {
+		throw new Error('Store name is required and must be an array of strings');
 	}
+
 	if (typeof version !== 'number') {
 		throw new Error('Version is required and must be a number');
 	}
